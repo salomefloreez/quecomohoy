db7f7a5486c4a525a9cfac204d1fd162de28f197
fcdg410 <fiorelladiaz410@gmail.com>
2024-12-24 10:00:09 +0100

    cambio chorra
    lol

--- app/src/main/java/com/example/quecomohoy/MainActivity.kt
+++ app/src/main/java/com/example/quecomohoy/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "tu puta madre $name!",
         modifier = modifier
     )
 }
