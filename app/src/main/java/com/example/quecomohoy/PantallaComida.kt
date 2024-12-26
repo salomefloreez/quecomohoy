d80d4c3c576d9c4126283bd8d7fc357b98b9280d
fcdg410 <fiorelladiaz410@gmail.com>
2024-12-26 19:13:34 +0100

    Creacion de clase de receta

---
+++ app/src/main/java/com/example/quecomohoy/PantallaComida.kt
@@ -0,0 +1,9 @@
+package com.example.quecomohoy
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PantallaComida(modifier: Modifier = Modifier) {
+    
+}
---
+++ app/src/main/java/com/example/quecomohoy/claseListaReceta/ClaseParaRecetas.kt
@@ -0,0 +1,9 @@
+package com.example.quecomohoy.claseListaReceta
+
+data class ClaseParaRecetas(
+    val titulo: String,
+    val tiempo: String,
+    val dificultad: Int,
+    val imagem: String,
+    val descripcion: String,
+)
