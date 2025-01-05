e19f718771d60607dfc45e2c048451e47e0bd1b9
chikitron17 <luciarodrigo04@gmail.com>
2025-01-05 20:09:38 +0100

    un mini cambio

--- app/src/main/java/com/example/quecomohoy/PantallaRecetaCompleta.kt
+++ app/src/main/java/com/example/quecomohoy/PantallaRecetaCompleta.kt
@@ -25,115 +25,122 @@ import coil.compose.rememberAsyncImagePainter
 @Composable
 fun PantallaRecetaCompleta() {
 
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .verticalScroll(rememberScrollState())
+            .background(Color(0xFFEEEEEE)) 
     ) {
 
-        Image(
-            painter = rememberAsyncImagePainter(model = "https://chefatulya.com/wp-content/uploads/2020/08/D129A118-8120-49AA-8445-ADBDBF7BA765.jpg"), // Cambia la URL por la de tu imagen
-            contentDescription = "Imagen de tallarines Udon",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
-        )
-
-        Box(
+        Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp)
-                .clip(RoundedCornerShape(30.dp))
-                .background(Color.White)
+                .verticalScroll(rememberScrollState())
         ) {
 
-
-            Column(
+            Image(
+                painter = rememberAsyncImagePainter(model = "https://chefatulya.com/wp-content/uploads/2020/08/D129A118-8120-49AA-8445-ADBDBF7BA765.jpg"), // Cambia la URL por la de tu imagen
+                contentDescription = "Imagen de tallarines Udon",
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
                     .padding(16.dp)
+                    .clip(RoundedCornerShape(30.dp))
+                    .background(Color.White)
             ) {
 
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
+
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
                 ) {
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "40-45 mins",
+                            fontSize = 14.sp,
+                            color = Color.Gray
+                        )
+                        Text(
+                            text = "Dificultad",
+                            fontSize = 14.sp,
+                            color = Color.Gray
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(8.dp))
                     Text(
-                        text = "40-45 mins",
+                        text = "Tallarines Udon",
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily(Font(R.font.alfa_slab_one))
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Text(
+                        text = "Ingredientes",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "4 raciones",
                         fontSize = 14.sp,
                         color = Color.Gray
+
                     )
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    val ingredientes = listOf(
+                        "2 bolsitas tallarines gordos precocidos tipo Udón",
+                        "1 filete grande de ternera",
+                        "1 bolsa congelada de setas y champiñones",
+                        "1 puerro",
+                        "1 zanahoria",
+                        "1/2 cebolla",
+                        "Salsa de soja",
+                        "Salsa barbacoa"
+                    )
+                    ingredientes.forEach { ingrediente ->
+                        Text(text = "• $ingrediente", fontSize = 14.sp)
+                    }
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
                     Text(
-                        text = "Dificultad",
-                        fontSize = 14.sp,
-                        color = Color.Gray
+                        text = "Preparación",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold
                     )
-                }
+                    Spacer(modifier = Modifier.height(8.dp))
 
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    text = "Tallarines Udon",
-                    fontSize = 30.sp,
-                    fontWeight = FontWeight.Bold,
-                    fontFamily = FontFamily(Font(R.font.alfa_slab_one))
-                )
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Text(
-                    text = "Ingredientes",
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold
-                )
-                Text(
-                    text = "4 raciones",
-                    fontSize = 14.sp,
-                    color = Color.Gray
-
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-
-                val ingredientes = listOf(
-                    "2 bolsitas tallarines gordos precocidos tipo Udón",
-                    "1 filete grande de ternera",
-                    "1 bolsa congelada de setas y champiñones",
-                    "1 puerro",
-                    "1 zanahoria",
-                    "1/2 cebolla",
-                    "Salsa de soja",
-                    "Salsa barbacoa"
-                )
-                ingredientes.forEach { ingrediente ->
-                    Text(text = "• $ingrediente", fontSize = 14.sp)
-                }
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Text(
-                    text = "Preparación",
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-
-                val pasos = listOf(
-                    "Picamos en juliana la cebolla y la ponemos a pochar en una sartén a fuego medio.",
-                    "Mientras se hace la cebolla, cortamos el puerro en tacos y lo añadimos a la sartén.",
-                    "Pelamos y rallamos la zanahoria y la añadimos a la sartén.",
-                    "Incorporamos la bolsa congelada de setas y champiñones y dejamos todo a fuego medio en sartén unos 15 minutos para que se haga bien.",
-                    "Mientras hacemos el filete de ternera a la plancha en otra sartén.",
-                    "Cortamos el filete en tiras no muy grandes y lo añadimos al sofrito.",
-                    "Echas un buen chorro de salsa barbacoa y un chorro de salsa de soja y dejamos un par de minutos.",
-                    "Si utilizas fideos udon precocidos, antes de añadirlos a la receta tienes que cocerlos en agua un par de minutos para que se suelten.",
-                    "Añadir los fideos y mezclarlo todo. Prueba el sabor para ajustar las salsas si es necesario."
-                )
-                pasos.forEachIndexed { index, paso ->
-                    Text(
-                        text = "${index + 1}. $paso",
-                        fontSize = 14.sp,
-                        modifier = Modifier.padding(bottom = 8.dp)
+                    val pasos = listOf(
+                        "Picamos en juliana la cebolla y la ponemos a pochar en una sartén a fuego medio.",
+                        "Mientras se hace la cebolla, cortamos el puerro en tacos y lo añadimos a la sartén.",
+                        "Pelamos y rallamos la zanahoria y la añadimos a la sartén.",
+                        "Incorporamos la bolsa congelada de setas y champiñones y dejamos todo a fuego medio en sartén unos 15 minutos para que se haga bien.",
+                        "Mientras hacemos el filete de ternera a la plancha en otra sartén.",
+                        "Cortamos el filete en tiras no muy grandes y lo añadimos al sofrito.",
+                        "Echas un buen chorro de salsa barbacoa y un chorro de salsa de soja y dejamos un par de minutos.",
+                        "Si utilizas fideos udon precocidos, antes de añadirlos a la receta tienes que cocerlos en agua un par de minutos para que se suelten.",
+                        "Añadir los fideos y mezclarlo todo. Prueba el sabor para ajustar las salsas si es necesario."
                     )
+                    pasos.forEachIndexed { index, paso ->
+                        Text(
+                            text = "${index + 1}. $paso",
+                            fontSize = 14.sp,
+                            modifier = Modifier.padding(bottom = 8.dp)
+                        )
+                    }
                 }
             }
         }
